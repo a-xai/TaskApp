1f24177b56bc042be1f1084c1c6df1f074ba3ef1
a-xai <akylaib007@gmail.com>
2021-04-10 15:08:11 -0700

    Initial commit

---
+++ app/src/main/java/com/example/taskapp/ui/FormFragment.java
@@ -0,0 +1,48 @@
+package com.example.taskapp.ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.example.taskapp.R;
+public class FormFragment extends Fragment {
+    private EditText editText;
+
+
+
+    private View formContainer;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_form, container, false);
+    }
+
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        editText = view.findViewById(R.id.edit_txt);
+        view.findViewById(R.id.btn_save).setOnClickListener(v -> {
+            save();
+        });
+
+
+    }
+
+    private void save() {
+        String text = editText.getText().toString();
+    }
+}
